97dd0c1aab49e9d7c819671db8eea7f53c895d2c
Johnson Jeremiah <johnsongoodjeremiah1@gmail.com>
2024-03-13 02:25:34 +0100

    modified:   README.md 	modified:   src/Components/Accordian/index.jsx

--- buildwith_react/src/Components/Accordian/index.jsx
+++ buildwith_react/src/Components/Accordian/index.jsx
@@ -3,18 +3,24 @@ import data from "./data";
 
 export default function Accordian() {
     const [selected, setSelected] = useState(null);
+    function handleSingleSelection(getCurrentId) {
+        setSelected(getCurrentId);
+    }
 
     return (
         <div className="wrapper">
             <div className="accordian">
                 {
                     data && data.length > 0 ? 
-                    data.map(dataItem => <div className="item">
-                        <div className="title">
+                    data.map((dataItem )=> (
+                    <div className="item">
+                        <div onClick={()=>handleSingleSelection(dataItem.id)} className="title">
                             <h4>{dataItem.question}</h4>
                             <span>+</span>
                         </div>
-                    </div>)
+                        
+                    </div>
+                    ))
                     : <div>Sorry, No Data Found !</div>
                 }
             </div>
